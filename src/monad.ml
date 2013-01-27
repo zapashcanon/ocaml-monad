de85a0b5db7eea35085353690a15def3853421b0
Phil Scott <me@philscotted.com>
2013-01-27 21:22:19 +0000

    Add error and continuation monads. Add modify in state. Fix naming of sum, msum and lsum. Add strict version of transpose. Change unique to take (=) by default.

--- src/monad.ml
+++ src/monad.ml
@@ -1,4 +1,6 @@
-open LazyList
+module Ll = LazyList
+let (^:^) = Ll.(^:^)
+let (^@^) = Ll.(^@^)
 
 module type BasePlus =
 sig
@@ -11,40 +13,40 @@ end
 module type BaseLazyPlus =
 sig
   include BatInterfaces.Monad
-  val zero : unit -> 'a m
+  val zero  : unit -> 'a m
   val lplus : 'a m -> 'a m Lazy.t -> 'a m
-  val null : 'a m -> bool
+  val null  : 'a m -> bool
 end
 
 module type Monad =
 sig
-  include BatInterfaces.Monad 
+  include BatInterfaces.Monad
   include Applicative.Applicative with type 'a m := 'a m
 
-  val (>>=) : 'a m -> ('a -> 'b m) -> 'b m
+  val (>>=)    : 'a m -> ('a -> 'b m) -> 'b m
   val join     : 'a m m -> 'a m
+  val filter_m : ('a -> bool m) -> 'a list -> 'a list m
 end
 
 module type MonadPlus =
 sig
-  include Monad
-  val zero  : unit -> 'a m
-  val plus : 'a m -> 'a m -> 'a m
-  val null  : 'a m -> bool
-  val filter : ('a -> bool) -> 'a m -> 'a m    
-  val of_list   : 'a list -> 'a m
-  val mconcat   : 'a m list -> 'a m    
+  include BasePlus
+  include Monad with type 'a m := 'a m
+  val filter  : ('a -> bool) -> 'a m -> 'a m
+  val of_list : 'a list -> 'a m
+  val sum       : 'a list m -> 'a m
+  val msum      : 'a m list -> 'a m
+  val transpose : 'a list m -> 'a m list
 end
 
 module type LazyPlus =
 sig
   include MonadPlus
-  val lplus     : 'a m -> 'a m Lazy.t -> 'a m
-  val of_llist  : 'a LazyList.t -> 'a m
-  val sum       : 'a LazyList.t m -> 'a m
-  val msum      : 'a m LazyList.t -> 'a m
-  val lsequence : 'a m LazyList.t -> 'a LazyList.t m
-  val transpose : 'a LazyList.t m -> 'a m LazyList.t
+  val lplus      : 'a m -> 'a m Lazy.t -> 'a m
+  val of_llist   : 'a LazyList.t -> 'a m
+  val lsum       : 'a LazyList.t m -> 'a m
+  val lmsum      : 'a m LazyList.t -> 'a m
+  val ltranspose : 'a LazyList.t m -> 'a m LazyList.t
 end
 
 module Make(M : BatInterfaces.Monad) =
@@ -61,10 +63,18 @@ struct
       include M
       let ap f x  = lift2 (fun f x -> f x) f x
     end)
-      
+
   include (Ap : Applicative.Applicative with type 'a m := 'a m)
 
-  let join m = m >>= (fun x -> x)
+  let join     m = m >>= (fun x -> x)
+
+  let filter_m p =
+    let rec loop m = function
+      | []      -> lift1 List.rev m
+      | (x::xs) -> loop (p x >>=
+                           fun b -> m >>=
+                               fun ys -> return (if b then (x::ys) else ys)) xs in
+    loop (return [])
 end
 
 module MakePlus(M : BasePlus) =
@@ -78,8 +88,15 @@ struct
   let of_list xs = List.fold_left
     (fun x y -> plus x (return y))
     (zero  ()) xs
-  let mconcat xs = List.fold_left plus
-    (zero  ()) xs
+  let sum xs =
+    xs >>=
+      (fun xs -> List.fold_right plus (List.map return xs) (zero ()))
+  let msum xs = List.fold_left plus (zero ()) xs
+
+  let rec transpose xs =
+    let hds = sum (lift1 (BatList.take 1) xs) in
+    if null hds then [] else
+      hds :: transpose (lift1 (BatList.drop 1) xs)
 end
 
 module MakeLazyPlus(M : BaseLazyPlus) =
@@ -89,27 +106,24 @@ struct
                      let plus x y = M.lplus x (lazy y)
                    end)
   let lplus = M.lplus
-  let sum xs =
+  let lsum xs =
     xs >>=
-      (fun xs -> fold_right lplus (zero ())
-        (map return xs))
+      (fun xs -> Ll.fold_right lplus (zero ())
+        (Ll.map return xs))
 
   let of_llist xs =
-    fold_right lplus (zero ())
-      (map return xs)
+    Ll.fold_right lplus (zero ())
+      (Ll.map return xs)
 
-  let lsequence ms = fold_left (lift2 (fun xs x -> x ^:^ xs))
-    (return nil) ms
-
-  let msum xs = fold_right lplus (zero ()) xs
+  let lmsum xs = Ll.fold_right lplus (zero ()) xs
 
   let filter p xs =
     xs >>= fun x -> if p x then return x else zero ()
-      
-  let rec transpose xs =
-    lazy (let hds = sum (lift1 (take 1) xs) in
-            if null hds then Nil else
-              Cons(hds, transpose (lift1 (drop 1) xs)))
+
+  let rec ltranspose xs =
+    lazy (let hds = lsum (lift1 (Ll.take 1) xs) in
+            if null hds then Ll.Nil else
+              Ll.Cons(hds, ltranspose (lift1 (Ll.drop 1) xs)))
 end
 
 module MakeStream(M : sig
@@ -122,58 +136,58 @@ struct
   type 'a t = 'a ML.m LazyList.node_t
   type 'a m = 'a t Lazy.t
 
-  let zero ()        = nil
-    
+  let zero () = Ll.nil
+
   let rec lplus xss yss =
-    let hd_yss = lazy (match next (Lazy.force yss) with
-	Nil -> ML.zero ()
-      | Cons (ys,_) -> ys)
-    and tl_yss = lazy (match next (Lazy.force yss) with
-	Nil -> nil
-      | Cons (_,yss) -> yss)
-    in lazy (match next xss with
-        Nil -> next (Lazy.force yss)
-      | Cons (xs,xss) ->                     
-        Cons (ML.lplus xs hd_yss, lplus xss tl_yss))
-    
+    let hd_yss = lazy (match Ll.next (Lazy.force yss) with
+        Ll.Nil -> ML.zero ()
+      | Ll.Cons (ys,_) -> ys)
+    and tl_yss = lazy (match Ll.next (Lazy.force yss) with
+        Ll.Nil -> Ll.nil
+      | Ll.Cons (_,yss) -> yss)
+    in lazy (match Ll.next xss with
+        Ll.Nil -> Ll.next (Lazy.force yss)
+      | Ll.Cons (xs,xss) ->
+        Ll.Cons (ML.lplus xs hd_yss, lplus xss tl_yss))
+
   let plus xss yss = lplus xss (lazy yss)
 
-  let null = is_empty
-    
-  let return x  = singleton (ML.return x)
-    
+  let null = Ll.is_empty
+
+  let return x  = Ll.singleton (ML.return x)
+
   let delay xs = M.zero () ^:^ xs
 
   let bind xs f =
     let join xs =
       let rec shift xs =
-    	lazy (match next xs with
-    	    Nil -> Nil
-          | Cons (x', xs') ->
-            next (plus
-                    (map ML.join (ML.transpose x'))
-                    (delay (shift xs')))) in
+        lazy (match Ll.next xs with
+            Ll.Nil -> Ll.Nil
+          | Ll.Cons (x', xs') ->
+            Ll.next (plus
+                       (Ll.map ML.join (ML.ltranspose x'))
+                       (delay (shift xs')))) in
       shift xs
-    in join (map (ML.lift1 f) xs)
+    in join (Ll.map (ML.lift1 f) xs)
 
   let ap fs xs =
     let rec shift fs =
-      lazy (match next fs with
-          Nil -> Nil
-        | Cons (f', fs') ->
-          next
+      lazy (match Ll.next fs with
+          Ll.Nil -> Ll.Nil
+        | Ll.Cons (f', fs') ->
+          Ll.next
             (* Without the null check, we'll have a space leak as
                the empty generation is uselessly applied across xs. *)
             (if ML.null f' then delay (shift fs')
              else plus
-                (map (M.ap f') xs)
+                (Ll.map (M.ap f') xs)
                 (delay (shift fs')))) in
     shift fs
-      
+
   let to_depth n = LazyList.take n
-           
+
   let rec iterate f xs =
-    plus xs (delay (lazy (next (iterate f (f xs)))))
+    plus xs (delay (lazy (Ll.next (iterate f (f xs)))))
 
 end
 
@@ -181,9 +195,9 @@ module type BaseCollectionM =
 sig
   include BaseLazyPlus
   val difference : ('a -> 'a -> bool) -> 'a m -> 'a m -> 'a m
-  val unique : ('a -> 'a -> bool) -> 'a m -> 'a m
+  val unique : ?cmp:('a -> 'a -> bool) -> 'a m -> 'a m
   val maxima : ('a -> 'a -> bool) -> 'a m -> 'a m
-  val nub : ('a -> 'a -> bool) -> 'a m -> 'a m    
+  val nub : ('a -> 'a -> bool) -> 'a m -> 'a m
 end
 
 module type Stream =
@@ -199,7 +213,6 @@ module type StreamC =
 sig
   include Stream
   include BaseCollectionM with type 'a m := 'a m
-  val lift_cmp : ('a -> 'a -> bool) -> ('a m -> 'a m) -> 'a m -> 'a m
 end
 
 module MakeStreamC(M : sig
@@ -214,34 +227,24 @@ struct
     let nub maxs x =
       let x' = M.nub p (M.difference p x maxs) in
         M.lplus (M.difference p maxs x') (lazy x'), x'
-    in map_accum_l nub (M.zero ()) xs
+    in Ll.map_accum_l nub (M.zero ()) xs
 
   let maxima p xs =
     let maxima maxs x =
       let x' = M.maxima p (M.difference p x maxs) in
         M.lplus (M.difference p maxs x') (lazy x'), x'
-    in map_accum_l maxima (M.zero ()) xs
-    
-  let unique p xs =
+    in Ll.map_accum_l maxima (M.zero ()) xs
+
+  let unique ?(cmp = (=)) xs =
     let unique uniques x =
-      let x' = M.difference p (M.unique p x) uniques
-      in M.lplus (M.difference p uniques x') (lazy x'), x'
-    in map_accum_l unique (M.zero ()) xs
+      let x' = M.difference cmp (M.unique ~cmp x) uniques
+      in M.lplus (M.difference cmp uniques x') (lazy x'), x'
+    in Ll.map_accum_l unique (M.zero ()) xs
 
-  let difference p xss yss =
-    let plus xs ys = M.lplus xs (lazy ys) in
-      zip_with (M.difference p)
-        xss
-        (LazyList.drop 1 (scan plus (M.zero ()) (yss ^@^ repeat (M.zero ()))))
-
-  let rec lift_cmp p f xs =
-    let rec ys = lazy
-      (let fys = delay (f ys) in
-       let xs = difference p xs fys in
-       let fys = difference p fys xs in
-       next (plus xs (delay fys)))
-    in ys
-
+  let difference p xss (yss : 'a m) =
+    Ll.zip_with (M.difference p)
+      xss
+      (Ll.drop 1 (Ll.scan Mm.plus (M.zero ()) (yss ^@^ Ll.repeat (M.zero ()))))
 end
 
 module LazyM =
@@ -252,7 +255,7 @@ struct
     lazy (Lazy.force (f (Lazy.force x)))
 end
 
-module Lazyt(M : BatInterfaces.Monad) =
+module LazyT(M : BatInterfaces.Monad) =
 struct
   module M  = Make(M)
   type 'a m = 'a Lazy.t M.m
@@ -266,7 +269,7 @@ module List =
 struct
   type 'a m = 'a list
   let return x  = [x]
-  let bind xs f = List.concat (List.map f xs)
+  let bind xs f = BatList.concat (List.map f xs)
   let zero () = []
   let plus xs ys = xs @ ys
   let null = function
@@ -274,7 +277,7 @@ struct
     | _  -> false
 end
 
-module Listt(M : BatInterfaces.Monad) =
+module ListT(M : BatInterfaces.Monad) =
 struct
   module M = Make(M)
   type 'a m = 'a list M.m
@@ -282,20 +285,20 @@ struct
   let bind xs f =
     M.bind xs (fun x ->
       M.lift1 BatList.concat
-        (M.sequence (BatList.map f x)))
+        (M.map_a f x))
 end
-
+  
 module LazyListM =
 struct
   type 'a m = 'a LazyList.t
-  let return x  = singleton x
-  let bind xs f = concat (map f xs)
-  let zero () = nil
+  let return x  = Ll.singleton x
+  let bind xs f = Ll.concat (Ll.map f xs)
+  let zero () = Ll.nil
   let rec lplus xs ys =
-    lazy (match next xs with
-        Nil        -> next (Lazy.force ys)
-      | Cons(x,xs) -> Cons(x, lplus xs ys))
-  let null = is_empty
+    lazy (match Ll.next xs with
+        Ll.Nil        -> Ll.next (Lazy.force ys)
+      | Ll.Cons(x,xs) -> Ll.Cons(x, lplus xs ys))
+  let null = Ll.is_empty
 end
 
 module Option =
@@ -308,13 +311,13 @@ struct
   let zero ()   = None
   let plus x y  =
     match x,y with
-	None, x        -> x
+        None, x        -> x
       | x, None        -> x
       | Some x, Some _ -> Some x
   let null      = BatOption.is_none
 end
 
-module Optiont(M : BatInterfaces.Monad) =
+module OptionT(M : BatInterfaces.Monad) =
 struct
   type 'a m = 'a option M.m
   let return x  = M.return (Some x)
@@ -323,6 +326,43 @@ struct
       (function
       None   -> M.return None
         | Some x -> f x)
+end
+
+module Error(E : sig type e val defaultError : e end) =
+struct
+  type 'a m = Error    of E.e
+              | Result of 'a
+
+  let return x = Result x
+  let bind x f = match x with
+      Error  e -> Error e
+    | Result x -> f x
+  let zero ()  = Error E.defaultError
+  let plus x y = match x with
+      Error  e -> y
+    | Result x -> Result x
+  let null x   = match x with
+      Error _  -> true
+    | _        -> false
+
+  let throw e = Error e
+
+  let catch x handler =
+    match x with
+      Error e  -> handler e
+    | Result x -> return x
+end
+
+module Continuation(T : sig type r end) =
+struct
+  type 'a m = ('a -> T.r) -> T.r
+
+  let return x k = k x
+  let bind c f k =
+    c (fun x -> (f x) k)
+
+  let callCC kk k =
+    kk (fun x -> fun _ -> k x) k
 end
 
 module State(T : sig type s end) =
@@ -334,9 +374,11 @@ struct
     f x s'
   let read s    = (s,s)
   let write x s = (x,())
+  let run x s   = x s
+  let modify f  = bind read (fun s -> write (f s))
 end
 
-module Statet(T : sig type s end)(M : BatInterfaces.Monad) =
+module StateT(T : sig type s end)(M : BatInterfaces.Monad) =
 struct
   type 'a m = T.s -> (T.s * 'a) M.m
   let return x s  = M.return (s, x)
@@ -344,8 +386,10 @@ struct
     M.bind (xf s) (fun (s',x) -> (f x) s')
   let read  s   = M.return (s,s)
   let write x s = M.return (x,())
+  let modify f  = bind read (fun s -> write (f s))
+  let run   x s = x s
 end
-  
+
 module type Monoid =
 sig
   type t
@@ -353,6 +397,26 @@ sig
   val plus : t -> t -> t
 end
 
+module type Reader =
+sig
+  type t
+  include BatInterfaces.Monad
+
+  val read : t m
+  val run  : t -> 'a m -> 'a
+end
+
+module MakeReader(M : sig type t end) =
+struct
+  include M
+  type 'a m = M.t -> 'a
+  let return x _ = x
+  let bind r f e =
+    f (r e) e
+  let read     e = e
+  let run e x    = x e
+end
+  
 module type Writer =
 sig
   type t
@@ -374,8 +438,8 @@ struct
   let run (x,y)    = (x,y)
   let write x      = (x,())
 end
-  
-module Writert(W : Writer)(M : BatInterfaces.Monad) =
+
+module WriterT(W : Writer)(M : BatInterfaces.Monad) =
 struct
   module M = Make(M)
   type t = W.t
@@ -402,20 +466,20 @@ end
 module CollectionOpt(C : BaseCollectionM) =
 struct
   module Option = Make(Option)
-  let liftp2 p x y =
-    match Option.lift2 p x y with
-        Some b -> b
-      | None   -> false
+  let liftp2 p x y = match x,y with
+      None, _        -> true
+    | Some x, None   -> false
+    | Some x, Some y -> p x y
 
-  include Optiont(C)
+  include OptionT(C)
   let zero () = C.zero ()
   let lplus xs ys = C.lplus xs ys
   let null xs = C.null xs
 
   let cmp_on p = liftp2 p
-  
+
   let difference p = C.difference (cmp_on p)
-  let unique p = C.unique (cmp_on p)
+  let unique ?(cmp = (=)) = C.unique ~cmp:(cmp_on cmp)
   let nub p = C.nub (cmp_on p)
   let maxima p = C.maxima (cmp_on p)
 end
@@ -425,7 +489,7 @@ module CollectionWriter(W : sig
   val cmp : t -> t -> bool
 end)(C : BaseCollectionM) =
 struct
-  include Writert(W)(C)
+  include WriterT(W)(C)
   let zero () = C.zero ()
   let lplus xs ys = C.lplus xs ys
   let null xs = C.null xs
@@ -433,12 +497,12 @@ struct
   let cmp_on p x y =
     let (vx,x) = W.run x in
     let (vy,y) = W.run y in
-    W.cmp vx vy && p x y
+    p x y && W.cmp vx vy
 
   let difference p = C.difference (cmp_on p)
-  let unique p = C.unique (cmp_on p)
+  let unique ?(cmp = (=)) = C.unique ~cmp:(cmp_on cmp)
   let maxima p = C.maxima (cmp_on p)
-  let nub p = C.nub (cmp_on p) 
+  let nub p = C.nub (cmp_on p)
 end
 
 module CollectionState(T :
@@ -447,15 +511,15 @@ module CollectionState(T :
     val cmp : s -> s -> bool
   end)(C : BaseCollectionM) =
 struct
-  include Statet(T)(C)
+  include StateT(T)(C)
   let zero () _ = C.zero ()
   let lplus xs ys s = C.lplus (xs s) (lazy (Lazy.force ys s))
   let null _ = false
 
-  let cmp_on p (s,x) (t,y) = T.cmp s t && p x y
-    
+  let cmp_on p (s,x) (t,y) = p x y && T.cmp s t
+
   let difference p xs ys s = C.difference (cmp_on p) (xs s) (ys s)
-  let unique p xs s = C.unique (cmp_on p) (xs s)
+  let unique ?(cmp = (=)) xs s = C.unique ~cmp:(cmp_on cmp) (xs s)
   let maxima p xs s = C.maxima (cmp_on p) (xs s)
   let nub p xs s = C.nub (cmp_on p) (xs s)
 

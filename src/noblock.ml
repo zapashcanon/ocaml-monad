951d961408eeab9fb6e4e5343d42f973208bcec3
Phil Scott <me@philscotted.com>
2014-08-19 10:04:43 +0100

    Add runnable interface.

--- src/noblock.ml
+++ src/noblock.ml
@@ -74,3 +74,50 @@ let write_line fd str =
       -> TryLater write, (inps,(pos+1,outs)), zero_w in
   fun (_,(pos,_) as s) -> TryLater write, s, ([],[AddWrite (pos,fd,str)])
 
+let fds_to_select fds =
+  let _,_,fds = BatList.fold_right
+                  (fun fd (i,seen,fds) ->
+                   if BatList.mem fd seen then
+                     i+1,seen,fds
+                   else
+                     i+1,fd::seen,((i,fd)::fds))
+                  fds (0,[],[]) in
+  fds
+
+let select in_fds out_fds =
+  BatList.(
+    let in_fds', out_fds', _ =
+      Unix.select (map snd in_fds) (map (fun (_,(_,fd)) -> fd) out_fds) [] (-1.0) in
+    filter (fun (_,fd) -> mem fd in_fds') in_fds,
+    filter (fun (_,(_,fd)) -> mem fd out_fds') out_fds)
+
+let rec read_all = function
+  | []                -> []
+  | (p,in_fd)::in_fds -> try let buf = BatString.make 10 (char_of_int 0) in
+                             let n = Unix.read in_fd buf 0 10 in
+                             (p,BatString.sub buf 0 n)::read_all in_fds
+                         with Unix.Unix_error (Unix.EAGAIN,_,_) -> read_all in_fds
+
+let rec write_all = function
+  | []                      -> []
+  | (p,(str,out_fd))::out_fds ->
+     try let _ = Unix.write out_fd str 0 (BatString.length str) in
+         p::write_all out_fds
+     with Unix.Unix_error (Unix.EAGAIN,_,_) -> write_all out_fds
+
+let rec update_ctx x = update_ctx x
+
+let run x =
+  let ctx = [],[] in
+  let rec run ctx x s =
+    match x s with
+    | Return y,((_,[]),(_,[])),([],[]) -> y
+    | Return _,(_,(_,[])),_            -> raise (Failure "dangling registrations")
+    | Return _,((_,[]),_),_            -> raise (Failure "dangling registrations")
+    | Return _,_,_                     -> raise (Failure "unused blocks")
+    | TryLater x,((_,[]),(_,[])),(inps,outs) ->
+       let in_fds,out_fds as ctx = update_ctx ctx inps outs in
+       let in_fds,out_fds = select (fds_to_select in_fds) (fds_to_select out_fds) in
+       run ctx x ((0,read_all in_fds),(0,write_all out_fds))
+    | TryLater _,_,_ -> raise (Failure "unused blocks") in
+  run ([],[]) x ((0,[]),(0,[]))

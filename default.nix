d845e680eb42017641758cd98fb613415043a669
Phil Scott <me@philscotted.com>
2016-03-21 08:54:38 +0000

    Initial commits.

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ pkgs ? (import <nixpkgs> {}).pkgs
+}:
+
+let
+  myemacs =
+    with pkgs.emacsPackages; pkgs.emacsWithPackages
+      [ haskellMode magit emacsw3m tuaregMode ];
+in with pkgs; stdenv.mkDerivation {
+  name = "monadlib";
+  buildInputs = [
+    ocamlPackages.findlib ocamlPackages.ocaml_batteries ocamlPackages.merlin
+    ocamlPackages.ocaml_oasis ocamlPackages.ocaml
+    opam myemacs ];
+  findlib = "${ocamlPackages.findlib}/lib/ocaml/4.01.0/site-lib/";
+  merlin  = "${ocamlPackages.merlin}";
+}
